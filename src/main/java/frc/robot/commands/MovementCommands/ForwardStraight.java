5c4d4072b475b40618f8089a11abdbfc59655e9f
alessandraGudoy <alessandraGudoy>
2023-03-04 09:21:24 -1000

    + added straightening code that uses field oriented logic

---
+++ src/main/java/frc/robot/commands/MovementCommands/ForwardStraight.java
@@ -0,0 +1,57 @@
+package frc.robot.commands.MovementCommands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.AutoConsts;
+import frc.robot.Constants.SwerveConsts;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class ForwardStraight extends CommandBase{
+    private final SwerveSubsystem swerve;
+    private double desiredEnc; 
+    private PIDController turningPID; 
+
+    public ForwardStraight(SwerveSubsystem newSwerve, double newDesiredEnc){
+        swerve = newSwerve;
+        desiredEnc = newDesiredEnc; 
+        turningPID = new PIDController(SwerveConsts.KP_TURNING, SwerveConsts.KI_TURNING, SwerveConsts.KD_TURNING);
+        turningPID.enableContinuousInput(-Math.PI, Math.PI); // System is circular;  Goes from -Math.PI to 0 to Math.PI
+
+        addRequirements(swerve);
+    }
+
+    @Override
+    public void initialize(){
+        swerve.resetEnc();
+    }
+
+    @Override
+    public void execute(){
+        SmartDashboard.putString("CurrentCommand", getName());
+
+        double turningSpeed = turningPID.calculate(swerve.getYaw(), 0);
+
+        ChassisSpeeds chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(AutoConsts.DRIVE_TRANSLATION_SPEED, 0, turningSpeed, swerve.getRotation2d());
+
+        // Convert chassis speeds to individual module states
+        SwerveModuleState[] moduleStates = SwerveConsts.DRIVE_KINEMATICS.toSwerveModuleStates(chassisSpeeds);
+
+        // Set each module state to wheels
+        swerve.setModuleStates(moduleStates);
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        SmartDashboard.putBoolean("drive fwd", false); 
+        swerve.stopModules();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return Math.abs(swerve.getDriveEnc()) > desiredEnc;
+    }
+
+}
