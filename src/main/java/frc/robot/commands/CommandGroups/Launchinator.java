1decad549edf23ddbe01030d350bbd921b0f1a81
alessandraGudoy <alessandraGudoy>
2023-03-04 09:31:33 -1000

    created command seq. to release rubber band mechanism at start of auto

---
+++ src/main/java/frc/robot/commands/CommandGroups/Launchinator.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.CommandGroups;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.ClawCommands.Claw;
+import frc.robot.commands.ClawCommands.ToStartingPosition;
+import frc.robot.commands.PivotCommands.PivotLowCommand;
+import frc.robot.commands.PivotCommands.TuckedIn;
+import frc.robot.subsystems.ClawSubsystem;
+import frc.robot.subsystems.PivotSubsystem;
+
+public class Launchinator extends SequentialCommandGroup {
+
+  public Launchinator(PivotSubsystem pivot, ClawSubsystem claw) {
+
+    // LAUNCH-INATE (RELEASE) RUBBER BAND 
+    addCommands(
+      new PivotLowCommand(pivot), 
+
+      new ToStartingPosition(claw), 
+
+      new TuckedIn(pivot)
+    );
+  }
+}
