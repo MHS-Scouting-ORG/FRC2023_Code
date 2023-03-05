5674a732b5104e723b37060c90c85717cd5dd9b7
KeaniKina <keani1028@gmail.com>
2023-03-05 12:53:38 -1000

    * Added limelight classes and Limelight tracking command

---
+++ src/main/java/frc/robot/commands/MovementCommands/LimelightRotate.java
@@ -0,0 +1,50 @@
+package frc.robot.commands.MovementCommands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.LimelightHelpers;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class LimelightRotate extends CommandBase{
+    private final SwerveSubsystem swerve;
+    private final Timer timer;
+
+    public LimelightRotate(SwerveSubsystem newSwerve){
+        swerve = newSwerve;
+        timer = new Timer();
+
+        addRequirements(swerve);
+    }
+
+    @Override
+    public void initialize(){
+        timer.reset();
+        timer.start();
+    }
+
+    @Override
+    public void execute(){
+        SmartDashboard.putBoolean("rotating", true);
+        
+        if (LimelightHelpers.getTX("limelight") < -1){
+            swerve.rotateLeft(0.1);
+        }
+        else if(LimelightHelpers.getTX("limelight") > 1){
+            swerve.rotateRight(0.1);
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        SmartDashboard.putBoolean("rotating", false); 
+        swerve.stopModules();
+        timer.stop();
+    }
+
+    @Override
+    public boolean isFinished(){
+        return LimelightHelpers.getTX("limelight") > -1 && LimelightHelpers.getTX("limelight") < 1;
+    }
+
+}
